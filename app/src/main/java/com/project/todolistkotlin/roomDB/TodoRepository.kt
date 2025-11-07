4b14686d485b38456135a3caba9b81522dfb7987
junho <junho5508@naver.com>
2025-11-07 22:35:48 +0900

    feat: `TodoRepository` 구현 및 싱글톤 패턴 적용
    데이터 소스를 관리하고 뷰 모델과 데이터베이스 간의 상호작용을 중개하는 `TodoRepository` 클래스를 추가합니다.
    
    주요 변경사항은 다음과 같습니다:
    - `TodoDao`를 사용하여 데이터베이스 작업을 수행합니다 (전체 데이터 조회, 추가, ID 기반 삭제).
    - `LiveData`를 사용하여 모든 할 일 데이터를 UI에 노출합니다.
    - `suspend` 함수를 통해 비동기 데이터베이스 작업을 지원합니다.
    - 싱글톤 패턴을 적용하여 애플리케이션 전체에서 단일 리포지토리 인스턴스를 유지합니다.

---
+++ app/src/main/java/com/project/todolistkotlin/roomDB/TodoRepository.kt
@@ -0,0 +1,33 @@
+package com.project.todolistkotlin.roomDB
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+
+
+class TodoRepository private constructor(private val dao: TodoDao) {
+    val allData: LiveData<List<TodoEntity>> = dao.getAllData()
+
+
+    suspend fun insertData(todoEntity: TodoEntity) {
+        dao.setInsertTodo(todoEntity)
+    }
+
+    suspend fun deleteDataById(id: Int) {
+        dao.deleteDataWhereId(id)
+    }
+
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoRepository? = null
+
+        fun getInstance(application: Application): TodoRepository =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: TodoRepository(
+                    TodoDataBase.getInstance(application).dao()
+                ).also { INSTANCE = it }
+            }
+    }
+}
+
+
